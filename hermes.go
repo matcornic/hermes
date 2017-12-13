535ed84b17d9df1c1c161756a8fe9c5102d47c24
Mathieu Cornic <mathieu.cornic@gmail.com>
2017-12-13 20:28:34 +0100

    Feat/dep update (#30)
    * feat: explicite use of blackfriday v2 for compatibilty purpose

--- hermes.go
+++ hermes.go
@@ -5,7 +5,7 @@ import (
 	"github.com/Masterminds/sprig"
 	"github.com/imdario/mergo"
 	"github.com/jaytaylor/html2text"
-	"github.com/russross/blackfriday"
+	"gopkg.in/russross/blackfriday.v2"
 	"html/template"
 )
 
